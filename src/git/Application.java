5831f75f380b7967254d7999b05ab23ebbc96f4e
Alisher Serikbayev <darkstar020543@gmail.com>
2022-03-07 21:01:30 +0600

    Added 'for' to sout

--- src/git/Application.java
+++ src/git/Application.java
@@ -2,8 +2,9 @@ package git;
 
 public class Application {
     public static void main(String[] args) {
-
-        System.out.println("Hello");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello");
+        }
 
     }
 }
